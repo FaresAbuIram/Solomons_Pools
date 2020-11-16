a0ab875b1ce1862661e03ef7b0a196b02005e2db
farisabu3ram <aburamf@gmail.com>
2020-11-16 23:17:15 +0200

    add splash screen

--- solomons_pools_flutter/lib/main.dart
+++ solomons_pools_flutter/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'mainPage.dart';
+import 'package:flare_splash_screen/flare_splash_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,6 +12,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Flutter Demo', home: MainPageStatefulWidget());
+    return MaterialApp(
+      home: SplashScreen.navigate(
+          name: "assets/images/welcome_page.flr",
+          next: MainPageStatefulWidget(),
+          height: double.infinity,
+          alignment: Alignment.center,
+          until: () => Future.delayed(Duration(seconds: 4)),
+          backgroundColor: Colors.white,
+          startAnimation: "welcome",
+        ),
+    );
   }
 }
